d45a0eb0b99d7635e09d216e2bfca277e3789b5e
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-04-06 16:52:47 +0530

    some changes

--- services/user.services.js
+++ services/user.services.js
@@ -176,6 +176,15 @@ static async getBooks(){
   }
 };
 
+static async getAllShopkeepers(){
+  try {
+    const shopkeepers = await registerModel.find({ UserType: 'Shopkeeper' });
+    return shopkeepers;
+  } catch (error) {
+    throw new Error('Error fetching shopkeepers');
+  }
+};
+
 
 // Handle file upload logic
 static uploadImage(req, res, next){
@@ -215,83 +224,7 @@ static async saveBookImage (req) {
 };
 
      
-    /* static async sendOTPEmail(EmailText){
-       const OTP = otpGenerator.generate(6,{lowerCaseAlphabets:false,upperCaseAlphabets:false,specialChars:false});
-       otpStore[EmailText] = { OTP, expires: Date.now() + 10 * 60 * 1000 };
-       console.log(`Your OTP is ${OTP}`);
-
-       // OAuth2 client setup
-const oauth2Client = new google.auth.OAuth2(
-  '73921812417-5vmpnvo4cvikejm7idhgnsoktu4u5r6q.apps.googleusercontent.com',   // Replace with your Google client ID
-  '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',  // Replace with your Google client secret
-  'http://localhost:5000/oauth2callback'  // Replace with your redirect URL
-);
-
-// Set credentials
-oauth2Client.setCredentials({
-  refresh_token: '1//046MSezgwdT7aCgYIARAAGAQSNwF-L9IrqQ45xaU_nHYIMYoXDFXtCg5GIjjgzEAq1mydAjANdoDufkGVQI06tMl4t_8S2Oku3RM'  // Replace with your refresh token
-});
-
-       const {AUTH_EMAIL,AUTH_PASS} = process.env
-        const transporter = nodemailer.createTransport({
-        port: 587,
-        secure: false,
-          service: 'gmail',
-          auth: {
-            type: 'OAuth2',
-            user: process.env.AUTH_EMAIL,  // Replace with your email
-            pass: process.env.AUTH_PASS, 
-            access_token: "ya29.a0ARW5m765elqiLNEGnaqHEMswi8K08itW_B_O2A2xQNGnadtfL2d2pgH-6NtPM3Fgg2qSy8cR8H5YsvcNRrM4UexZQCIq8-PdoHychXciLXLqa-p_NM1HXVrB4FWCMMVawXnmf_BUHuuDHXY_Ce-cACMHbzRkOhosPP6zOdGTaCgYKAa4SARMSFQHGX2MiOaGGjUvvC4IimPfx6vT31w0175",  // Replace with your email password
-            refresh_token: "1//046MSezgwdT7aCgYIARAAGAQSNwF-L9IrqQ45xaU_nHYIMYoXDFXtCg5GIjjgzEAq1mydAjANdoDufkGVQI06tMl4t_8S2Oku3RM",
-            clientId:'73921812417-5vmpnvo4cvikejm7idhgnsoktu4u5r6q.apps.googleusercontent.com',
-            clientSecret:'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',
-          },
-          logger: true,  // Enable logging
-          debug: true
-        });
-       //test transporter
-        transporter.verify((error, success) => {
-          if (error) {
-            console.error('SMTP Transporter Error:', error);
-          } else {
-            console.log('SMTP connection is ready to send emails!');
-          }
-        });
-
-        const mailOptions = {
-            from: process.env.AUTH_EMAIL,
-            to: EmailText,
-            subject: 'Password Reset OTP',
-            text: `Your OTP for resetting your password is: ${OTP}`,
-          };
-        
-          await transporter.sendMail(mailOptions);
-          res.status(200).send('OTP sent to your email');
-        } catch (error) {
-          console.error(error);
-          res.status(500).send('Error sending OTP');
-
-         //Save otp record
-         const hashOTP = async(generateOTP,saltRounds = 10)=>{
-          try{
-            const hashDta = await bcrypt.hash(generateOTP,saltRounds);
-            return hashOTP;
-          }catch(error){
-            throw error;
-          }
-         }
-         const newOTP =  new OTPModel({
-          EmailText,
-          otp : hashOTP,
-          createAt : Date.now(),
-          expireAt : Date.now() + 5*60*1000 ,
-         });
-          const createdOTPRecord = newOTP.save();
-          return createdOTPRecord;
-
-        }*/
-
-
+   
           static async sendOTPEmail(EmailText,subject,message,duration=1){
           try{
             if(!(EmailText && subject && message)){
@@ -323,22 +256,6 @@ oauth2Client.setCredentials({
           }
           };
       
-          /*static async sendOTPEmail(EmailText,subject,message,duration=1){
-            try{
-             if(!EmailText && !subject && !message){
-              throw Error("Provide value for email, subject, message ");
-             }
-
-             //clear any old record
-             await OTPModel.deleteOne({EmailText});
-             const OTP =generateOTP();
-
-             //Send Email
-
-            }catch(error){
-             
-            }
-          }*/
 
         static async verifyOTP(EmailText, otp){
           const otpRecord = otpStore[EmailText];
@@ -352,36 +269,7 @@ oauth2Client.setCredentials({
           }
         }
 
-        /*static async sendOTPEmail(params,callback){
-          const otp = otpGenerator.generate(6,{lowerCaseAlphabets:false,upperCaseAlphabets:false,specialChars:false});
-          const ttl = 5*60*1000;
-          const expires = Date.now() + ttl;
-          const data = `${params.EmailText}.${otp}.${expires}`;
-          const hash = crypto.createHmac("sha256",Key).update(data).digest("hex");
-          const fullhash = `${hash}.${expires}`;
-          console.log(`Your OTP is ${otp}`);
-
-          //Send SMS
-          return callback(null,fullhash);
-
-        }*/
-
-       /* static async verifyOTP(params,callback){
-          console.log('params.hash:', params.hash);   
-          if (!params.hash) {
-          return res.status(400).json({ error: 'Missing or invalid hash' });
-          }
-         let [hashValue,expires] = params.hash.split('.');
-         let now = Date.now();
-         if(now > parseInt(expires)) return callback("OTP Expired");
-          let data = `${params.EmailText}.${params.otp}.${expires}`;
-          let newCalculateHash = crypto.createHmac("sha256",Key).update(data).digest("hex");
-
-          if(newCalculateHash == hashValue){
-            return callback(null,"Success");
-          }
-          return callback("Invalid OTP");
-        }*/
+       
 }
 
 
