04d5f892324099170ed10e380ede1dad3413be8e
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-07-30 20:12:55 +0530

    mongodb atlas link

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect("mongodb://localhost:27017/reuseBook");
+const connect = mongoose.connect("mongodb+srv://mahakguptaji2005:Lq1yG4ry04fBsSjO@reusebook-cluster.zip2ptq.mongodb.net/?retryWrites=true&w=majority&appName=reusebook-cluster");
 const registerModel = require('../models/register_model');
 
 // Set up GridFS stream
