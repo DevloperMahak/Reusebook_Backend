a16f2bb9140afd2f8603832e8c8ac51412eb18ab
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-07-30 23:19:21 +0530

    New mongobd atlas uri

--- config/db.js
+++ config/db.js
@@ -1,18 +1,20 @@
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect("mongodb+srv://mahakguptaji2005:Lq1yG4ry04fBsSjO@reusebook-cluster.zip2ptq.mongodb.net/?retryWrites=true&w=majority&appName=reusebook-cluster");
-const registerModel = require('../models/register_model');
 
-// Set up GridFS stream
-const conn = mongoose.connection;
 
 //check database connected or not
-const connection = connect.then(()=>{
-    console.log("Database connected successfully.");
-})
-.catch(()=>{
-    console.log("Database not connected successfully.");
-})
+const connection = async () => {
+    try {
+      await mongoose.connect("mongodb+srv://mahakguptaji2005:Lq1yG4ry04fBsSjO@reusebook-cluster.zip2ptq.mongodb.net/?retryWrites=true&w=majority&appName=reusebook-cluster", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("✅ Database connected successfully.");
+    } catch (error) {
+      console.error("❌ Database connection failed:", error.message);
+      process.exit(1);
+    }
+  };
 
 
 module.exports = connection;
