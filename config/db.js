93a83f9a46ad4e147c44db031c2842ae558a1a59
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-08-02 20:08:17 +0530

    some changes

--- config/db.js
+++ config/db.js
@@ -5,10 +5,7 @@ const mongoose = require("mongoose");
 //check database connected or not
 const connection = async () => {
     try {
-      await mongoose.connect("mongodb+srv://mahakguptaji2005:Lq1yG4ry04fBsSjO@reusebook-cluster.zip2ptq.mongodb.net/?retryWrites=true&w=majority&appName=reusebook-cluster", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+      await mongoose.connect("mongodb+srv://mahakguptaji2005:Lq1yG4ry04fBsSjO@reusebook-cluster.zip2ptq.mongodb.net/?retryWrites=true&w=majority&appName=reusebook-cluster");
       console.log("✅ Database connected successfully.");
     } catch (error) {
       console.error("❌ Database connection failed:", error.message);
