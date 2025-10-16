e062b5b188b8eae7621823390cd8247cc5325f90
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-10-16 12:15:41 +0530

    CORS updated

--- src/app.js
+++ src/app.js
@@ -10,9 +10,22 @@ const path = require('path');
 
 const app = express();
 
+const allowedOrigins = [
+  'https://reusebook-frontend.onrender.com', // deployed frontend
+  'http://localhost:5000', // backend testing
+  'http://localhost:3000', // Flutter Web default
+  'http://localhost:8080'
+];
+
 // ✅ Allow requests from frontend (CORS should come early)
 app.use(cors({
-  origin: 'https://reusebook-frontend.onrender.com',  // ✅ Replace with your deployed frontend
+  origin: function(origin, callback){
+    if (!origin || allowedOrigins.indexOf(origin) !== -1){
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
