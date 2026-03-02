3ba1259b6aa7070e08d3c0ef00ef5b7b5c309431
DeveloperMahak <mahakguptaji2005@gmail.com>
2026-03-03 00:03:37 +0530

    order fix

--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
-
-const app = require('./app');
-require('dotenv').config();
+require("dotenv").config();
+const app = require("./app");
 const mongoose = require("mongoose");
-const path = require('path');
-const connectdb = require('../config/db');
-const registerModel = require('../models/register_model');
+const path = require("path");
+const connectdb = require("../config/db");
+const registerModel = require("../models/register_model");
 
-
-
-
-
-const usersData=[];
+const usersData = [];
 
 connectdb(); // This will print DB connection status
 const PORT = process.env.PORT || 5000;
 
-
-app.get('/',(req,res)=>{
-  res.send('hello');
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
-    
-
